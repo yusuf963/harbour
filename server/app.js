d3dbfced61aad2fcb99f7626cf36280ee58f977a
yusuf963 <mohammad963yusuf@gmail.com>
2021-08-20 22:05:11 +0100

    import route to app

--- server/app.js
+++ server/app.js
@@ -1,15 +1,43 @@
 const express = require('express');
 require('dotenv/config');
+const morgan = require('morgan');
+const mongoos = require('mongoose');
+const cors = require('cors');
+
+//import routes files
+const itemRouter = require('./views/item');
+const userRouter = require('./views/user');
+const categoryRouter = require('./views/category');
 
 const app = express();
+app.use(cors());
+app.options('*', cors());
+app.use(express.json());
+app.use(morgan('tiny'));
 const api = process.env.API_URL;
 
-app.get('/', (req, res) => {
-  res.send('Server is Running 🚀🚀🚀👨‍🎤👨‍🎤');
-});
+// API routes
+app.use('/items', itemRouter);
+app.use('/users', userRouter);
+app.use('/category',categoryRouter)
 
+// Connect to MongoDB
+mongoos
+  .connect(process.env.DB_CONNECTION_STRING, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    dbName: 'harbour',
+  })
+  .then(() => {
+    console.log('Connected to DB');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+// Start server
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-    console.log(api)
+  console.log(api);
   console.log(`Listening on port ${port}`);
 });
