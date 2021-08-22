6486144df26e6b58f42ee5daf12ae862f2a0cfa7
yusuf963 <mohammad963yusuf@gmail.com>
2021-08-22 15:43:25 +0100

    import helper functions and ceate order route

--- server/app.js
+++ server/app.js
@@ -3,23 +3,29 @@ require('dotenv/config');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const authJwt = require('./helpers/jwt');
+const errorHandler = require('./helpers/error-handler');
 
 //import routes files
 const itemRouter = require('./views/item');
 const userRouter = require('./views/user');
 const categoryRouter = require('./views/category');
+const orderRouter = require('./views/order');
 
 const app = express();
 app.use(cors());
 app.options('*', cors());
 app.use(express.json());
 app.use(morgan('tiny'));
+app.use(authJwt());
+app.use(errorHandler);
 const api = process.env.API_URL;
 
 // API routes
 app.use('/items', itemRouter);
 app.use('/users', userRouter);
 app.use('/category', categoryRouter);
+app.use('/orders', orderRouter);
 
 // Connect to MongoDB
 mongoose.set('useCreateIndex', true);
